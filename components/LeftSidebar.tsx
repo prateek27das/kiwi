f4f649544178f822664271f76858c747aa436bc8
Prateek Das <prateek17das@gmail.com>
2024-11-12 23:25:59 +0600

    Implemented constants and added routing and labels

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,3 +1,4 @@
+import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,9 +7,23 @@ const LeftSidebar = () => {
   return (
     <section className="left_Sidebar">
       <nav className="flex flex-col gap-6">
-        <Link href="/">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Podcaster
+          </h1>
         </Link>
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          return (
+            <Link href={route} key={label} className="flex gap-3">
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
